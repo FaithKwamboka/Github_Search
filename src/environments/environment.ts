27dfba32d6575927ac7c99c9d66a0052dc332cc6
FaithKwamboka <okongofaith3@gmail.com>
2022-04-16 00:40:31 +0300

    Add token and Api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  token: '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',
+  githubApi: 'https://api.github.com',
 };
 
 /*
