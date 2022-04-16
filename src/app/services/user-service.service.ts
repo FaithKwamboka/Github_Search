db292b5a6be5b48b9c3d8dce71d1308422480a46
FaithKwamboka <okongofaith3@gmail.com>
2022-04-17 00:43:07 +0300

    Updating the service

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserClass } from '../classes/user-class';
+import { RepoClass } from '../classes/repo-class';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+  username: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.username = 'FaithKwamboka';
+  }
+  getUser() {
+    return this.http.get('https://api.github.com/users/' + this.username)
+    .pipe(map(result => result));
+  }
+  getRepos() {
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos')
+      .pipe(map(result => result));
+  }
+  myUpdate(username:string){
+    this.username = username
+  }
 }
