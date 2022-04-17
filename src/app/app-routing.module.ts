ea801f294d0d8e0579f7f2068d367f3df9135a66
FaithKwamboka <okongofaith3@gmail.com>
2022-04-18 01:16:41 +0300

    Adding page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landingpage', component:LandingPageComponent},
+  {path: 'user', component:LandingPageComponent},
+  {path: 'repositories', component:LandingPageComponent},
+  {path: '', component:LandingPageComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
